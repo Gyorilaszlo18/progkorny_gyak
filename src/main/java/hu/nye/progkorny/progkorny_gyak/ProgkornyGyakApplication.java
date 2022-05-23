0059bb8af2b236a06181b71d20aa019a986a84da
Gyorilaszlo18 <gyorilaszlo18@gmail.com>
2022-05-23 14:19:46 +0200

    gyakorlás2

---
+++ src/main/java/hu/nye/progkorny/progkorny_gyak/ProgkornyGyakApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.progkorny.progkorny_gyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgkornyGyakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgkornyGyakApplication.class, args);
+	}
+
+}
